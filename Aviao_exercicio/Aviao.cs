9728a69516c4603dfe7dec8b69fea251564c9c72
Pietro Guerra <100379604+projetosguerra@users.noreply.github.com>
2023-03-15 14:57:04 -0300

    Add files via upload

---
+++ Aviao_exercicio/Aviao.cs
@@ -0,0 +1,16 @@
+namespace aviao.exercicio;
+class Aviao
+{
+    public string modelo = "";
+    public string marca = "";
+    public int altitude;
+    public int velocidade;
+
+    public void acelerar(){
+
+    }
+    public void reduzir(){
+
+    }
+    public void subir(){}
+}
---
+++ Aviao_exercicio/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace aviao.exercicio;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
