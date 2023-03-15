7d630dfaec1aa7fb05edd3462e8613c2344cf888
Pietro Guerra <100379604+projetosguerra@users.noreply.github.com>
2023-03-15 14:56:08 -0300

    Add files via upload

---
+++ Aluno-nota/Alunos.cs
@@ -0,0 +1,34 @@
+namespace exercicio;
+class Alunos
+{
+    public string nome="";
+
+    public double nota1, nota2;
+
+    //MEDIA -> retorna um double (por exemplo um número como 8.3)
+
+    public double obterMedia(){
+        double media= (nota1+nota2)/2;
+        return media;
+    }
+
+    //SITUAÇÂO -> retorna uma string ("Aprovado" ou "Reprovado")
+
+    public string obterSituacao(double media){
+        string situacao="";
+        if(media>=6){
+            situacao="Aprovado";
+        }else{
+            situacao="Reprovado";
+        }
+        return situacao;
+    }
+
+    //MENSAGEM -> não retorna nada. Só mostra na tela os detalhes (nome, média e situação)
+
+    public void mensagem(){
+        double mediaCalculada = obterMedia();
+        string resultadoSituacao = obterSituacao(mediaCalculada);
+        Console.WriteLine(nome+" está "+resultadoSituacao+" com média "+mediaCalculada);
+    }
+}
---
+++ Aluno-nota/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace exercicio;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Alunos aluno1 = new Alunos();
+        aluno1.nome = "Pietro Guerra";
+        aluno1.nota1 = 8;
+        aluno1.nota2 = 5;
+
+        aluno1.mensagem();
+    }
+}
