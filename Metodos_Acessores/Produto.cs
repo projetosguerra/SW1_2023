145214fb6d07e42d3685b713c4cd984a09102355
Pietro Guerra <100379604+projetosguerra@users.noreply.github.com>
2023-03-29 16:30:52 -0300

    Add files via upload

---
+++ Metodos_Acessores/Produto.cs
@@ -0,0 +1,27 @@
+namespace Metodos_Acessores;
+class Produto
+{
+
+    private string? Nome {get;set;}
+
+    private double Preco {get;set;}
+
+
+    public void MostraDados(){
+        Console.WriteLine("Nome: " + this.Nome );
+        Console.WriteLine("Preco: " + this.Preco );
+    }
+
+    public void AlteraPreco(double preco){
+        if (preco >= 0) {
+            this.Preco = preco;
+        }else {
+            Console.WriteLine("O preço está indisponível");
+        }
+    }
+
+    public void AlteraNome(string nome){
+        this.Nome = nome;
+    }
+
+}
